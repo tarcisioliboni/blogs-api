33f8d29d42cb15eef2367f000b366b35836cfae5
Tarcisio Dev <tarcisioliboni@pop-os.localdomain>
2022-07-25 20:11:39 -0300

    Request 07 OK "model category created"

---
+++ src/database/models/category.js
@@ -0,0 +1,21 @@
+const createCategory = (sequelize, DataTypes) => {
+  const categories = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: { 
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+    tableName: 'Categories'
+  });
+  return categories;
+};
+
+module.exports = createCategory;
